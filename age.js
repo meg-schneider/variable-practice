4c8bb757e06aaa3def7fd6ea6ab1e85021c343ba
Meg Schneider <megschneider616@gmail.com>
2021-10-04 00:27:40 -0400

    ask for user input

--- age.js
+++ age.js
@@ -1,5 +1,6 @@
-let age = 32;
-console.log(`In 10 years, you will be ${age + 10} years old.`);
-console.log(`In 20 years, you will be ${age + 20} years old.`);
-console.log(`In 30 years, you will be ${age + 30} years old.`);
-console.log(`In 40 years, you will be ${age + 40} years old.`);
+let rlSync = require('readline-sync');
+let age = rlSync.question("How old are you?\n");
+console.log(`In 10 years, you will be ${Number(age) + 10} years old.`);
+console.log(`In 20 years, you will be ${Number(age) + 20} years old.`);
+console.log(`In 30 years, you will be ${Number(age) + 30} years old.`);
+console.log(`In 40 years, you will be ${Number(age) + 40} years old.`);
