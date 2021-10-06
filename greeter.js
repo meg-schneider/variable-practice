21aa8be335a4c75a62bceb096da63881e3e88aa6
Meg Schneider <megschneider616@gmail.com>
2021-10-06 09:48:15 -0400

    Add a function to this program that solicits the user's first and last names in separate invocations

--- greeter.js
+++ greeter.js
@@ -1,4 +1,12 @@
-let firstName = 'Victor';
-console.log(`Good morning, ${firstName}.`);
-console.log(`Good afternoon, ${firstName}.`);
-console.log(`Good evening, ${firstName}.`);
+// let firstName = 'Victor';
+function getName(prompt) {
+  let rlSync = require('readline-sync');
+  let name = rlSync.question(prompt);
+  return(name);
+}
+
+let firstName = getName("What's your first name?\n");
+let lastName = getName("What's your last name?\n");
+console.log(`Good morning, ${firstName} ${lastName}.`);
+console.log(`Good afternoon, ${firstName} ${lastName}.`);
+console.log(`Good evening, ${firstName} ${lastName}.`);
